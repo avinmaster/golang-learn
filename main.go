801fb1174d06a2a990c87c8d7aa50bf9a8cb1680
avinmaster <oybek.odili@gmail.com>
2021-11-04 21:00:35 +0500

    Initial commit

--- main.go
+++ main.go
@@ -6,10 +6,17 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	w.Header().Set("Content-Type", "text/html")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	} else if r.URL.Path == "/contact" || r.URL.Path == "/contact/" {
+		fmt.Fprint(w, "<a href=\"mailto:support@test.com\">support@test.com</a>")
+	} else {
+		fmt.Fprint(w, "<h1>404!</h1>")
+	}
 }
 
 func main() {
 	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe("localhost:3000", nil)
+	http.ListenAndServe(":3000", nil)
 }
